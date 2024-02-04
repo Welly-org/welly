d84318082021cb2d7e7f3afc3103f502410174f7
Linh <dangdiemlinh04@gmail.com>
2024-02-03 21:43:10 -0500

    Profile page

--- client/app/profileOthers/page.tsx
+++ client/app/profileOthers/page.tsx
@@ -29,7 +29,7 @@ const ProfileOthers = () => {
     name: "",
     groups: [],
   });
-  const [posts, setPosts] = useState<UserPosts[]>([]);
+  const [posts, setPosts] = useState<Post[]>([]);
 
   const getUser = async () => {
     try {
@@ -38,8 +38,6 @@ const ProfileOthers = () => {
         name: res.data.username,
         groups: res.data.groups,
       });
-      console.log("posts")
-      console.log(posts);
     } catch (err) {
       console.log(err);
     }
@@ -48,23 +46,21 @@ const ProfileOthers = () => {
   const getPosts = async () => {
     try {
       let res = await axios.get(`http://localhost:4000/users/${id}/posts`);
-      setPosts([
-        {
-        posts: res.data.posts.map((post: Post) => ({
-          id: post.id, 
+      
+      setPosts((prevPosts) => [
+        ...prevPosts,
+        ...res.data.posts.map((post: Post) => ({
+          id: post.id,
           photo: post.photo,
         })),
-      },
-    ]);
-      console.log(res.data);
+      ]);
+
     } catch (err) {
       console.log(err);
     }
   };
 
   useEffect(() => {
-    console.log("FROM USEEFFECT");
-
     getUser();
     getPosts(); 
   }, []);
@@ -92,8 +88,8 @@ const ProfileOthers = () => {
         </Flex>
         <div className="mt-4 text-winered">{user.name}</div>
       </Flex>
-      <div className="text-darkbrown mt-11 text-3xl">{user.name}'s groups</div>
-      <Flex direction="column" className="mt-4">
+      <div className="text-darkbrown mt-6 text-3xl">{user.name}'s groups</div>
+      <Flex direction="column">
         {user.groups.map((group, index) => (
           <div key={index}>
             <Flex className="bg-orange text-2xl mt-6 py-3 text-darkbrown w-80 px-7 rounded-full">
@@ -102,20 +98,21 @@ const ProfileOthers = () => {
           </div>
         ))}
       </Flex>
-      {/* <div className="grid grid-cols-3 gap-4">
-        {posts.posts.map((post, index) => (
+      <div className="mt-6 grid grid-cols-3 gap-2">
+        {posts.map((post, index) => {
+          console.log(posts)
+          return(
           <div
             key={index}
-            className="relative overflow-hidden aspect-w-1 aspect-h-1"
+            className="relative rounded w-24 h-24 overflow-hidden aspect-w-1 aspect-h-1"
           >
             <img
               src={post.photo}
-              alt={`Post ${index}`}
               className="absolute top-0 left-0 w-full h-full object-cover"
             />
           </div>
-        ))}
-      </div> */}
+        )})}
+      </div>
     </Flex>
   );
 };
